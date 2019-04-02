ebc8a9ee09d2ab4f9d8c0c7ac6030582b0709091
Julia Marco <marcootj@gmail.com>
2019-04-02 17:54:26 -0600

    Added routes for auth/google and sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'home#index'
   get '/login', to: 'login#index'
-
+  get 'auth/google', as: :google_connect
+  get 'auth/google/callback',  to: 'sessions#create'
 
 end
