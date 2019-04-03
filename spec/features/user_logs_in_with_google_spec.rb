75a008f3847ea2a2e14e501af8f0d71effaef954
Julia Marco <marcootj@gmail.com>
2019-04-02 20:57:38 -0600

    Added final functionality so that both the model and feature tests are passing

--- spec/features/user_logs_in_with_google_spec.rb
+++ spec/features/user_logs_in_with_google_spec.rb
@@ -2,23 +2,15 @@ require 'rails_helper'
 
 RSpec.feature 'user logs in' do
   it 'authenticating through Google' do
-
     visit '/'
 
     expect(current_path).to eq('/login')
 
     click_button 'Connect to Google'
 
-    expect(current_path).to eq('/home')
+    expect(current_path).to eq('/')
 
     expect(page).to have_content('Welcome, Julia!')
-
-
-
-
-
-    # If I am already logged in as a user, when I visit “/”, I see the home screen.
-    # This story includes setup of the User model and database table. Please use your best judgment about which fields to store in the DB.
   end
 
 end
