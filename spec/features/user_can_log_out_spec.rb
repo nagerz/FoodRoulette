b66935f6dbe82073dfe3e81869997777998a26dd
Chris Lewis <13chlewis@gmail.com>
2019-04-03 18:12:51 -0600

    User logout functionality tested and implemented

---
+++ spec/features/user_can_log_out_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'When a logged in user visits their profile page' do
+  it 'they can log out of the app' do
+    visit root_path
+
+    click_button 'Connect to Google'
+
+    expect(current_path).to eq(root_path)
+
+    click_button 'Profile'
+
+    expect(current_path).to eq(profile_path)
+
+    click_button 'Log Out'
+
+    expect(current_path).to eq(login_path)
+
+    visit root_path
+
+    expect(current_path).to eq(login_path)
+  end
+end
