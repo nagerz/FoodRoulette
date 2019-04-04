f1a671bf774d11deabcd81b4ae66fa1c4d5249af
nagerz <znager@gmail.com>
2019-04-04 16:56:33 -0600

    Added thumbnail and refresh token to user table
    Co-authored-by: April Dagonese <41272635+aprildagonese@users.noreply.github.com>
    Co-authored-by: Chris Lewis <41928817+csvlewis@users.noreply.github.com>
    Co-authored-by: Julia Marco <35079289+juliamarco@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_04_225051) do
+ActiveRecord::Schema.define(version: 2019_04_04_225535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,8 @@ ActiveRecord::Schema.define(version: 2019_04_04_225051) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role", default: 0
+    t.text "refresh_token"
+    t.text "thumbnail"
   end
 
   create_table "visits", force: :cascade do |t|
