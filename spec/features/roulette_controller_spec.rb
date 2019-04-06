8e411a47ca3764c95678d39ff36cbee3ce1565a8
Julia Marco <35079289+juliamarco@users.noreply.github.com>
2019-04-06 13:03:51 -0600

    Update roulette_controller_spec.rb

--- spec/features/roulette_controller_spec.rb
+++ spec/features/roulette_controller_spec.rb
@@ -15,7 +15,7 @@ describe RouletteController, type: :controller do
     expect(response.status).to eq(200)
   end
 
-  it 'the user\'s manal location can be used for a default random or group roulette', :vcr do
+  it 'the user\'s manual location can be used for a default random or group roulette', :vcr do
     user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     cookies['manual_location'] = 'Denver, CO'
