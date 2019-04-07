ff1b30545ed118c714398ff149fb9909392c15c0
nagerz <znager@gmail.com>
2019-04-07 13:05:00 -0600

    Added survey methods in order to act on incoming vote response

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -5,4 +5,45 @@ class Survey < ApplicationRecord
   attr_accessor :sender, :event, :date_time, :restaurant_1, :restaurant_2, :restaurant_3
 
   enum status: %i[active inactive]
+
+  #vote model
+  def find_survey
+    #most recent survey where incoming number is one of the outgoing numbers
+  end
+
+  #vote model
+  def unique_vote?
+    #true if all survey.votes.number do not match the incoming number
+  end
+
+  #vote model
+  def add_survey_restaurant(response, survey)
+    index = response.to_i - 1
+    survey.survey_restaurants[index]
+    survey_restaurant.update_attribute(:vote, vote)
+  end
+
+  #instance method on found survey
+  def check_status
+    numbers_sent = phone_numbers.split(",").count
+    if votes.count = numbers_sent
+      end_survey
+    end
+  end
+
+  def end_survey
+    update_attribute(:status, 1)
+    tally_ranks
+    #close channel?
+  end
+
+  def tally_ranks
+    #select survey_restaurants where survey = survey
+    #order by total vote count desc
+    survey_restaurants.sort_by {|restaurant| restaurant.votes.count }
+    survey_restaurants.each do |survey_restaurant|
+      rank = survey_restaurants.index(survey_restaurant)
+      update_attribute(:rank, rank)
+    end
+  end
 end
