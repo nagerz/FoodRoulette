f19fa2ee36899fcb6e1720018ff130175bd405a4
Chris Lewis <13chlewis@gmail.com>
2019-04-11 11:06:53 -0600

    Commented out skylight

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
-require "skylight"
+# require "skylight"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -23,7 +23,7 @@ module FoodRoulette
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.active_job.queue_adapter = :sidekiq
-    config.skylight.environments += ["development"]
+    # config.skylight.environments += ["development"]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
