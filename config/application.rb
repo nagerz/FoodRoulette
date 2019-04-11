b8864e25c79042df6f021402614126e727bdf470
nagerz <znager@gmail.com>
2019-04-10 18:19:31 -0600

    skylight update

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module FoodRoulette
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.active_job.queue_adapter = :sidekiq
-    config.skylight.environments += ["staging"]
+    config.skylight.environments += ["development"]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
